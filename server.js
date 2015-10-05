721a5ae708109e43e5d069a3cac3b99f10cc12d2
Cocoon Daniels <johnhyde@ArtMac2.local>
2015-10-05 08:25:22 -0700

    who even knows anymore

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var app = express();
-var port = 8080;
+var app = express(), api = express();
+var port = 8080, apiPort = 8081;
 require('./router/main')(app);
+require('./router/api')(api);
 app.set('views',__dirname + '/views');
 //app.use(express.staticProvider(__dirname));
 app.set('view engine', 'jade');
@@ -11,4 +12,7 @@ app.engine('.jade', require('jade').__express);
 //});
 var server = app.listen(port,function() {
     console.log("We have started our server on port " + port);
+})
+var apiServer = api.listen(apiPort,function() {
+    console.log("We have started our api server on port " + apiPort);
 })
