ae491e77c8535befcb8bf7ab3db22abbb5a1eb5a
Cocoon Daniels <johnhyde@ArtMac2.local>
2015-10-29 09:41:26 -0700

    helpful commit message

--- jadeGen.py
+++ jadeGen.py
@@ -4,12 +4,10 @@ f = []
 for (dirpath, dirnames, filenames) in walk('./views'):
     f.extend(filenames)
     break
-print f
 for wat in f:
-    print wat
     if (".html" in wat):
-        print wat.find(".html")
         filename = "views/" + wat[0:wat.find(".html")] + ".jade"
         file = open(filename, "w")
         file.write("include " + wat)
         file.close()
+print "Jade files generated."
