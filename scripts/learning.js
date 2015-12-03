c82dc5e76ba1c390474e32a5b5d21226db56aa6e
Period 1 <period1@ArtMac1.local>
2015-12-03 08:57:00 -0800

    some data visualization options

--- scripts/learning.js
+++ scripts/learning.js
@@ -1,14 +1,17 @@
+
+
 var arrayX = [0],
     arrayY = [0],
     arrayZ = [0];
 
-for (var i = 0; i < 1000; i++)
+for (var i = 0; i < 100; i++)
 {
     arrayX[i] = Math.random() * 2000 - 1000;
     arrayY[i] = Math.random() * 2000 - 1000;
-    arrayZ[i] = arrayX[i] * arrayY[i] - arrayX[i] * arrayX[i] + .25 * arrayY[i] * arrayY[i];
+    arrayZ[i] = Math.cos(arrayX[i]/500) * Math.sin(arrayY[i]/500);
 };
 
+/*                    3D SURFACE GRAPH
 var data = [{
     x: arrayX,
     y: arrayY,
@@ -18,10 +21,11 @@ var data = [{
         size: 12,
         opacity: 0.1
     },
-    type: 'scatter3d'
+    type: 'surface'           //change to 'contour' for a heatmap style
 }];
 
 var layout = {
   title: 'Mt Bruno Elevation',
 };
 Plotly.newPlot('myDiv', data, layout);
+*/
