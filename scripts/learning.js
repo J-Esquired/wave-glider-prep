e779a95fa369ddbdfff2cbcb30b4a39a4334eab9
Period 1 <period1@ArtMac1.local>
2015-12-03 09:51:04 -0800

    refined data stuffs

--- scripts/learning.js
+++ scripts/learning.js
@@ -1,5 +1,37 @@
+/*
+//                 3D SCATTER PLOT, NICE TEXTURE
+var arrayX = [0],
+    arrayY = [0],
+    arrayZ = [0];
 
+for (var i = 0; i < 10000; i++)
+{
+    arrayX[i] = Math.random() * 2000 - 1000;
+    arrayY[i] = Math.random() * 2000 - 1000;
+    arrayZ[i] = Math.cos(arrayX[i]/500) * Math.sin(arrayY[i]/500);
+};
 
+var data = [{
+    x: arrayX,
+    y: arrayY,
+    z: arrayZ,
+    mode: 'markers',
+    marker: {
+        size: 12,
+        opacity: 0.1
+    },
+    type: 'scatter3d'
+}];
+
+var layout = {
+  title: 'Mt Bruno Elevation',
+};
+Plotly.newPlot('myDiv', data, layout);
+
+*/
+/////////////////////////////////////////////////////////////////////////////
+/*
+//                    CONTOUR PLOT, IS QUITE SLOW
 var arrayX = [0],
     arrayY = [0],
     arrayZ = [0];
@@ -11,7 +43,6 @@ for (var i = 0; i < 100; i++)
     arrayZ[i] = Math.cos(arrayX[i]/500) * Math.sin(arrayY[i]/500);
 };
 
-/*                    3D SURFACE GRAPH
 var data = [{
     x: arrayX,
     y: arrayY,
@@ -21,11 +52,44 @@ var data = [{
         size: 12,
         opacity: 0.1
     },
-    type: 'surface'           //change to 'contour' for a heatmap style
+    type: 'contour'
 }];
 
 var layout = {
   title: 'Mt Bruno Elevation',
 };
 Plotly.newPlot('myDiv', data, layout);
+
+*/
+/////////////////////////////////////////////////////////////////////////////
+/*
+//                    
+var arrayX = [0],
+    arrayY = [0],
+    arrayZ = [0];
+
+for (var i = 0; i < 100; i++)
+{
+    arrayX[i] = Math.random() * 2000 - 1000;
+    arrayY[i] = Math.random() * 2000 - 1000;
+    arrayZ[i] = Math.cos(arrayX[i]/500) * Math.sin(arrayY[i]/500);
+};
+
+var data = [{
+    x: arrayX,
+    y: arrayY,
+    z: arrayZ,
+    mode: 'markers',
+    marker: {
+        size: 12,
+        opacity: 0.1
+    },
+    type: 'contour'
+}];
+
+var layout = {
+  title: 'Mt Bruno Elevation',
+};
+Plotly.newPlot('myDiv', data, layout);
+
 */
